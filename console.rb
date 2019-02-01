530a88f3cdec5fc289537cbf18d1f4e117381766
ghr85 <ghrenfrew@gmail.com>
2019-02-01 16:45:53 +0000

    initial setup commit

---
+++ console.rb
@@ -0,0 +1,16 @@
+#Codeclan Week 03 Day 05 Homework
+# Codeclan Cinema
+# Relational Databases and Ruby
+# Class
+
+# Don't worry about an interface for this one just use PRY
+
+require('pry')
+require_relative('db/sql_runner')
+require_relative('models/customer')
+require_relative('models/film')
+require_relative('models/screening')
+require_relative('models/ticket')
+
+binding.pry
+nil
